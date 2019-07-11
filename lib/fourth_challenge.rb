ef2d24128d100e9fca67f1d4583ea5550f6cfdc1
Chris Hodge <chrishodge87@gmail.com>
2019-07-11 18:00:34 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,16 +7,16 @@ def fourth_challenge
       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
                         {name: "Mercutio", age: "17", attitude: "hot-headed"}]
         #your hashes here!
-      ]
+      
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
-                           {name: "Nurse", age: "44", attitude:"worried"}]
+                           {name: "Nurse", age: "44", attitude: "worried"}]
         #your hashes here!
-      ]
+      
    }
 }
 
